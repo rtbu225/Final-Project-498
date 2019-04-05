2b2511c94e25e22e31ec236d49a800931b0ce60e
rtbu225 <36251108+rtbu225@users.noreply.github.com>
2019-04-05 12:29:04 -0400

    Update home.php
    I added comments to this.

--- home.php
+++ home.php
@@ -1,8 +1,11 @@
 <!DOCTYPE html>
+<!--home.php, the main page and file for our program. Creates the DOM for rateMyClasses -->
 <html>
+    <!--Main header div. Should be displayed at the top of the page.-->
     <h1> Rate My Class </h1>
     <br> <br> <br> <br>
     <h3> Submit Review </h3>
+    <!--Submit review div. Is linked to submit_review.php. Handles the logic for writing and submitting a review for a class. -->
     <div id="submit_review" name="submit_review">
         <form action='submit_review.php' method="get">
             <a> class: </a>
@@ -20,6 +23,7 @@
     <br> <br> <br> <br>
 
     <h3> Search Reviews for Specific Class </h3>
+    <!-- search reviews div. Handles the logic for searching for a specific class' reviews. Linked to fetch_classes.php -->
     <div id="search_reviews" name="search_reviews">
         <form action="fetch_classes.php" method="get">
             <a> department name: </a>
