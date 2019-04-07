2696e4e504e5e5f3d9e37fdb86ad897fe7b04eca
rtbu225 <36251108+rtbu225@users.noreply.github.com>
2019-04-07 15:19:23 -0400

    Update fetch_classes.php
    Comments

--- fetch_classes.php
+++ fetch_classes.php
@@ -1,3 +1,7 @@
+<!--fetch_classes.php: requires home.php (the main page) to be accessible. 
+ -                      gets the information for a given class. 
+                        Values are hardcoded right now, but in the end will be populated by input from the DB -->
+
 <?php
     require "home.php";
 ?>
