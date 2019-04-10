54a48fbc0f678eaab4d0d8773fed9c86efd23b94
Luke Andrew <43147015+landrew26@users.noreply.github.com>
2019-04-10 11:05:16 -0400

    Set up database connection for Diego to implement

--- submit_review.php
+++ submit_review.php
@@ -2,6 +2,21 @@
 
 <?php
     // TODO: SUBMIT CLASS, DEPARTMENT, AND REVIEW TO DATABASE
+    error_reporting(0);
+    session_start();
+
+    $mysqli = new mysqli('sql103.epizy.com', 'epiz_23744465', 'ZJjYMvI29', 'epiz_23744465_project');
+
+    if ($mysqli->connect_errno) {
+        echo "Could not connect to database \n";
+        echo "Error: ". $mysqli->connect_error . "\n";
+        exit;
+    } else {
+        if (isset($_POST['submit_review'])) {
+            // TODO: CHANGE THESE QUERIES
+            // $query = "SELECT username, password FROM login WHERE username = " . "'$username'";
+        }
+    }
 ?>
 
 <html>
