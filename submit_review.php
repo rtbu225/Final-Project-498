32f702da0dcddc610e7ad5197e55b25304fc22a1
Luke Andrew <43147015+landrew26@users.noreply.github.com>
2019-04-19 11:02:49 -0400

    complete implementation of function

--- submit_review.php
+++ submit_review.php
@@ -1,9 +1,13 @@
-<!-- submit_review.php, Handles the logic for submitting a review to the database.  -->
-
 <?php
     // TODO: SUBMIT CLASS, DEPARTMENT, AND REVIEW TO DATABASE
     error_reporting(0);
     session_start();
+
+    $class=$_POST['class'];
+    $department=$_POST['department'];
+    $professor=$_POST['professor'];
+    $review=$_POST['review'];
+
 
     $mysqli = new mysqli('sql103.epizy.com', 'epiz_23744465', 'ZJjYMvI29', 'epiz_23744465_project');
 
@@ -12,13 +16,18 @@
         echo "Error: ". $mysqli->connect_error . "\n";
         exit;
     } else {
-        if (isset($_POST['submit_review'])) {
-            // TODO: CHANGE THESE QUERIES
-            // $query = "SELECT username, password FROM login WHERE username = " . "'$username'";
+        $query = "INSERT INTO reviews (class, department, professor, review) VALUES ('$class', '$department', '$professor', '$review')";
+        $q_result = $mysqli->query($query);
+        
+        if ($q_result->num_rows === 0) {
+            echo "Did not successfully insert the information!\n";
+        } else {
+//            echo "Your information has been received! $class $department $professor $review";
+            readfile("submitReview.html");
         }
     }
 ?>
 
-<html>
-    <h1> submitting a review </h1>
-</html>
+<!--<html>-->
+<!--    <a href="http://cs498project.epizy.com/home.php">home</a>-->
+<!--</html>	-->
